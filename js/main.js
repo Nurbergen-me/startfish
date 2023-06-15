6fdce1ac360b193e582950c22fa5ea54af7b5d24
nurik.eleshov <nurik.eleshov@gmail.com>
2023-06-15 16:18:15 +0800

    pages && mobile version

--- js/main.js
+++ js/main.js
@@ -4,15 +4,28 @@ const stepsCirle = document.querySelectorAll('.steps_circle')
 if (steps) {
     window.addEventListener('scroll', function() {
         const viewportOffset = steps.getBoundingClientRect();
-        
-        if (viewportOffset.top > -200 && viewportOffset.top < viewportOffset.height) {
-            stepsCirle.forEach((elem, index) => {
-                if (Math.round((viewportOffset.top + 300) / ((viewportOffset.height) / 5)) == 5 - index) {
-                    elem.classList.add('active')
-                } else {
-                    elem.classList.remove('active')
-                }
-            })
+
+        if (this.innerWidth > 992) {
+            if (viewportOffset.top > -200 && viewportOffset.top < viewportOffset.height) {
+                stepsCirle.forEach((elem, index) => {
+                    if (Math.round((viewportOffset.top + 300) / ((viewportOffset.height) / 5)) == 5 - index) {
+                        elem.classList.add('active')
+                    } else {
+                        elem.classList.remove('active')
+                    }
+                })
+            }
+        } else {
+            if (viewportOffset.top + viewportOffset.height > 0) {
+                stepsCirle.forEach((elem, index) => {
+                    if (viewportOffset.top < this.innerHeight - ((viewportOffset.height * (index+1)) / 5)) {
+                        elem.classList.add('active')
+                    } else {
+                        elem.classList.remove('active')
+                    }
+                })
+            }
         }
+        
     })
 }
