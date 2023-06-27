bff722ac27f7edf3d36424c94cf3596e4e472a1c
nurik.eleshov <nurik.eleshov@gmail.com>
2023-06-27 17:07:02 +0800

    vue router

---
+++ pages/about.vue.js
@@ -0,0 +1,3 @@
+var About = {
+	template: "<div><h1>About</h1><p>This is home page</p></div>"
+};
---
+++ pages/contacts.vue.js
@@ -0,0 +1,3 @@
+var Contacts = {
+	template: "<div><h1>Contacts</h1><p>This is home page</p></div>"
+};
